91b7c6ce826fce3f70cfcc2bdde1c9f385352f63
SpaceOtterInSpace <jacqnelson@gmail.com>
2014-09-20 14:01:30 -0500

    level 4

--- levelcode.rb
+++ levelcode.rb
@@ -27,4 +27,20 @@ class Player
 end
 
 #level 4
+class Player
+  def play_turn(warrior)
+    if !warrior.feel.empty?
+        warrior.attack!
+    elsif warrior.health < 20
+      if @health && warrior.health < @health
+        warrior.walk!
+      else
+        warrior.rest!
+      end
+    else
+        warrior.walk!
+    end
+    @health = warrior.health
+   end
+end
 
