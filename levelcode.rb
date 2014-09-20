a4180ee808679bd94fe078924a4aa36fb9ae8057
SpaceOtterInSpace <jacqnelson@gmail.com>
2014-09-20 14:17:34 -0500

    adding comments

--- levelcode.rb
+++ levelcode.rb
@@ -29,16 +29,16 @@ end
 #level 4
 class Player
   def play_turn(warrior)
-    if !warrior.feel.empty?
+    if !warrior.feel.empty? #when a monster is next to you
         warrior.attack!
-    elsif warrior.health < 20
-      if @health && warrior.health < @health
+    elsif warrior.health < 20 #when there is no monster next to you but your health is less than 20
+      if @health && warrior.health < @health #when youre getting shot by the arrow
         warrior.walk!
-      else
+      else #when no one is attacking this brings your health up to 20
         warrior.rest!
       end
     else
-        warrior.walk!
+        warrior.walk! #when no one is attacking and your health is 20 you need to keep walking
     end
     @health = warrior.health
    end
