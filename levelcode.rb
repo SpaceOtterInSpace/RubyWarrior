435877aace3cd081a7d1990e37a9dfc96aa565a3
SpaceOtterInSpace <jacqnelson@gmail.com>
2014-09-20 13:01:37 -0500

    Level 3

--- levelcode.rb
+++ levelcode.rb
@@ -12,3 +12,19 @@ class Player
 end
 
 #level 3
+class Player
+  def play_turn(warrior)
+    if warrior.feel.empty?
+      if warrior.health < 20
+        warrior.rest!
+      else
+        warrior.walk!
+      end
+    else
+      warrior.attack!
+    end
+  end
+end
+
+#level 4
+
